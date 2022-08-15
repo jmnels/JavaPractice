9d232e477fa16d9c3655fabd8ac08f06e82c1130
Joshua Nelson <jmn2020@hotmail.com>
2022-08-15 19:18:25 -0400

    Polymorphism begin

---
+++ src/PolymorphismExample/Vehicle.java
@@ -0,0 +1,16 @@
+package PolymorphismExample;
+
+public class Vehicle {
+
+    protected String start;
+    protected String stop;
+    protected String speed;
+    protected String direction;
+
+    public Vehicle(String start, String stop, String speed, String direction) {
+        this.start = start;
+        this.stop = stop;
+        this.speed = speed;
+        this.direction = direction;
+    }
+}
